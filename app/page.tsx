f1b12134a9cad571d92f49c2032c2c150774092e
maharathi31 <hriday.rathi@plaksha.edu.in>
2024-02-05 21:35:32 +0530

    Realtime reset and delete functionality implemented

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
-import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { defaultNavElement } from "@/constants/index";
+import { handleCanvaseMouseMove, handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified, handleResize, initializeFabric, renderCanvas } from "@/lib/canvas";
+import { handleDelete } from "@/lib/key-events";
+import { useMutation, useStorage } from "@/liveblocks.config";
 import { ActiveElement } from "@/types/type";
 import {fabric} from "fabric"
 import {useEffect, useRef, useState} from 'react'
@@ -15,14 +18,65 @@ export default function Page() {
   const isDrawing=useRef(false)
   const shapeRef=useRef<fabric.Object | null>(null)
   const selectedShapeRef=useRef<string | null>('rectangle')
+  const activeObjectRef=useRef<fabric.Object | null>(null)
   const [activeElement,setActiveElement]=useState<ActiveElement>({
     name:'',
     value:'',
     icon:''
   })
 
+  const deleteAllShapes= useMutation(({storage})=>{
+    const canvasObjects=storage.get('canvasObjects')
+
+    if(!canvasObjects || canvasObjects.size==0)
+    return true
+
+    for(const[key] of canvasObjects.entries()){
+      canvasObjects.delete(key)
+    }
+
+    return canvasObjects.size==0
+
+  },[])
+  const canvasObjects=useStorage((root)=>root.canvasObjects)
+
+  const syncShapeInStorage=useMutation(({storage},object)=>{
+    if(!object) return
+
+    const {objectId}=object
+    const shapeData=object.toJSON()
+    shapeData.objectId=objectId
+
+    const canvasObjects=storage.get('canvasObjects')
+
+    canvasObjects.set(objectId,shapeData)
+  },[])
+
+  const deleteShapeFromStorage=useMutation(({
+    storage
+  },objectId)=>{
+    const canvasObjects=storage.get('canvasObjects')
+    canvasObjects.delete(objectId)
+  },[])
+
   const handleActiveElement=(elem:ActiveElement)=>{
     setActiveElement(elem)
+
+    switch(elem?.value){
+      case 'reset':
+        deleteAllShapes()
+        fabricRef.current?.clear()
+        setActiveElement(defaultNavElement)
+        break
+      
+      case 'delete':
+      handleDelete(fabricRef.current as any,
+        deleteShapeFromStorage) 
+        setActiveElement(defaultNavElement)
+      default:
+        break
+    }
+
     selectedShapeRef.current=elem?.value as string
   }
   useEffect(()=>{
@@ -38,11 +92,53 @@ export default function Page() {
       })
     })
 
+    canvas.on("mouse:move",(options)=>{
+      handleCanvaseMouseMove({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage
+      })
+    })
+
+    canvas.on("mouse:up",(options)=>{
+      handleCanvasMouseUp({
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+        setActiveElement,
+        activeObjectRef
+      })
+    })
+
+    canvas.on("object:modified",(options)=>{
+      handleCanvasObjectModified({
+        options,
+        syncShapeInStorage,
+      })
+    })
+
+
   window.addEventListener("resize",()=>{
     handleResize({fabricRef})
   })
+
+  return ()=>{
+    canvas.dispose()
+  }
   },[])
 
+  useEffect(()=>{
+    renderCanvas({
+      fabricRef,
+      canvasObjects,
+      activeObjectRef
+    })
+  })
   
   return (
       <div className="h-screen overflow-hidden">
