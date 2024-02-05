9722f856ae6465419760b1b33a2790ffac1b617f
maharathi31 <hriday.rathi@plaksha.edu.in>
2024-02-05 17:13:37 +0530

    Navbar functionality complete

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
 import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
 import {fabric} from "fabric"
-import {useEffect, useRef} from 'react'
+import {useEffect, useRef, useState} from 'react'
 
 export default function Page() {
 
@@ -14,7 +15,16 @@ export default function Page() {
   const isDrawing=useRef(false)
   const shapeRef=useRef<fabric.Object | null>(null)
   const selectedShapeRef=useRef<string | null>('rectangle')
+  const [activeElement,setActiveElement]=useState<ActiveElement>({
+    name:'',
+    value:'',
+    icon:''
+  })
 
+  const handleActiveElement=(elem:ActiveElement)=>{
+    setActiveElement(elem)
+    selectedShapeRef.current=elem?.value as string
+  }
   useEffect(()=>{
     const canvas=initializeFabric({canvasRef,fabricRef})
 
@@ -36,7 +46,9 @@ export default function Page() {
   
   return (
       <div className="h-screen overflow-hidden">
-      <Navbar/>
+      <Navbar
+      activeElement={activeElement}
+      handleActiveElement={handleActiveElement}/>
       <section className="flex h-full flex-row">
       <LeftSidebar/>
       <Live canvasRef={canvasRef}/>
